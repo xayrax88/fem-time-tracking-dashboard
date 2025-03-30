a6c4aabd2dba5c3cc111a0c2214d46e6a87083c0
Anouxay Sichanh <ainthaly@gmail.com>
2025-03-30 15:43:28 -0400

    started JS functionality

---
+++ script.js
@@ -0,0 +1,10 @@
+const dailyButton = document.querySelector('.daily-btn');
+const weeklyButton = document.querySelector('.weekly-btn');
+const monthlyButton = document.querySelector('.monthly-btn');
+const currentHours = document.querySelector('.current-hrs');
+
+
+
+dailyButton.addEventListener('click', () => {
+
+})

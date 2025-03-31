6249122d9231e0982347dc04f90ba0f316c0ccd3
Anouxay Sichanh <ainthaly@gmail.com>
2025-03-31 11:33:45 -0400

    JS UI Fn & fetch API

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ const monthlyBtn = document.querySelector('.monthly-btn');
 const currentHrs = document.querySelector('.current-hrs');
 const previousHrs = document.querySelector('.previous-hrs');
 
-fetch('/data.json').then((response) => {
-    if (!response.ok) return console.log('Data was not fetched');
+let jsonData = [];
+fetch('data.json')
+    .then((response) => {
+        if (!response.ok) throw new Error('Data was not fetched')
+        //return console.log('Data was not fetched');
+        return response.json();
+    })
+    .then((data) => {
+        json = data;
+        //console.log(json[0]);
+        updateUI('daily'); //update daily data UI
+    })
+    .catch(error => console.log(error));
 
-    return response.json();
-}).then((data) => {
-    console.log(data);
-})
+function updateUI(timeframe) {
+    jsonData.forEach((item, index) => {
+        currentHrs[index].textContent = `Current - ${item.timeframes[timeframe].current} hours`;
+        previousHrs[index].textContent = `Last week - ${item.timeframes[timeframe].previous} hours`
+    })
+}
 
-
-// dailyBtn.addEventListener('click', () => {
-
-// })
+dailyBtn.addEventListener('click', () => updateUI('daily'));
+weeklyBtn.addEventListener('click', () => updateUI('weekly'));
+monthlyBtn.addEventListener('click', () => updateUI('monthly'));

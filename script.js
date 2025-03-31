2514fd420b31f8f56304aa5d3b3ae8c935156327
Anouxay Sichanh <ainthaly@gmail.com>
2025-03-31 17:27:38 -0400

    finished mobile user card

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const dailyBtn = document.querySelector('.daily-btn');
-const weeklyBtn = document.querySelector('.weekly-btn');
-const monthlyBtn = document.querySelector('.monthly-btn');
+const dailyLink = document.querySelector('.daily-link');
+const weeklyLink = document.querySelector('.weekly-link');
+const monthlyLink = document.querySelector('.monthly-link');
 const allCurrentHrs = document.querySelectorAll('.current-hrs');
 const allPreviousHrs = document.querySelectorAll('.previous-hrs');
+const links = [dailyLink, weeklyLink, monthlyLink];
 
 let jsonData = [];
 fetch('data.json')
@@ -13,18 +14,55 @@ fetch('data.json')
     })
     .then((data) => {
         jsonData = data
-        //console.log(jsonData[0]);
+        //console.log(jsonData);
         updateUI('daily'); //Display daily data by default
+        setActiveLink(dailyLink); //highlight default active link
     })
     .catch(error => console.log(error));
 
-function updateUI(timeframe) {
+function updateUI(hours) {
+    if (!jsonData.length) return;
+
     jsonData.forEach((item, index) => {
-        allCurrentHrs[index].textContent = `${item.timeframes[timeframe].current} Hrs`;
-        allPreviousHrs[index].textContent = `Last week - ${item.timeframes[timeframe].previous} Hrs`
+        allCurrentHrs[index].textContent = `${item.timeframes[hours].current} Hrs`;
+        allPreviousHrs[index].textContent = `Last week - ${item.timeframes[hours].previous} Hrs`
     })
 }
 
-dailyBtn.addEventListener('click', () => updateUI('daily'));
-weeklyBtn.addEventListener('click', () => updateUI('weekly'));
-monthlyBtn.addEventListener('click', () => updateUI('monthly'));
+function setActiveLink(activeLink) {
+    links.forEach(link => link.classList.remove('selected-link'));
+
+    activeLink.classList.add('selected-link');
+}
+
+links.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let hours;
+        if (link === dailyLink) {
+            hours = 'daily';
+        } else if (link === weeklyLink) {
+            hours = 'weekly';
+        } else if (link === monthlyLink) {
+            hours = 'monthly';
+        }
+        updateUI(hours);
+        setActiveLink(link)
+    })
+})
+
+//Adding event listener to btns
+// dailyBtn.addEventListener('click', () => updateUI('daily'));
+// weeklyBtn.addEventListener('click', () => updateUI('weekly'));
+// monthlyBtn.addEventListener('click', () => updateUI('monthly'));
+
+//adding eventlistener to a element links
+// [dailyLink, weeklyLink, monthlyLink].forEach(link => {
+//     link.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         updateUI(link.classList.contains('daily-link') ? 'daily' :
+//                  link.classList.contains('weekly-link') ? 'weekly' : 'monthly');
+//     });
+// });
+
